2667a58c8f75d238cd8a7540423e3bf0ccf462ef
Celal <binaycelalselim@gmail.com>
2025-01-19 23:51:08 +0300

    main docs added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import CardsContainer from "./components/cards/CardsContainer";
+import MyHeader from "./components/header/MyHeader";
+
+function App() {
+  return (
+    <div>
+      <MyHeader />
+      <CardsContainer />
+    </div>
+  );
+}
+
+export default App;
